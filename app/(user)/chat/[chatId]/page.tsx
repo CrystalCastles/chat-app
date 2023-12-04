fdfaed046a22d9a468228915cc937c7237be5876
Daniel Pichardo <danielpichardo716@gmail.com>
2023-12-04 11:14:35 -0500

    lots of functionality integrations

--- app/(user)/chat/[chatId]/page.tsx
+++ app/(user)/chat/[chatId]/page.tsx
@@ -1,12 +1,38 @@
-function ChatPage() {
+import { authOptions } from "@/auth";
+import AdminControls from "@/components/AdminControls";
+import ChatInput from "@/components/ChatInput";
+import ChatMembersBadges from "@/components/ChatMembersBadges";
+import ChatMessages from "@/components/ChatMessages";
+import { sortedMessagesRef } from "@/lib/converters/Message";
+import { getDocs } from "firebase/firestore";
+import { getServerSession } from "next-auth";
+
+type Props = {
+  params: {
+    chatId: string;
+  };
+};
+
+async function ChatPage({ params: { chatId } }: Props) {
+  const session = await getServerSession(authOptions);
+
+  const initialMessages = (await getDocs(sortedMessagesRef(chatId))).docs.map(
+    (doc) => doc.data()
+  );
   return (
     <>
-    {/* Admin Controls */}
-    {/* ChatMembersBadge */}
-    {/* ChatMessages */}
-    {/* ChatInput*/}
+      <AdminControls chatId={chatId} />
+      <ChatMembersBadges chatId={chatId} />
+      <div className="flex-1">
+        <ChatMessages
+          chatId={chatId}
+          session={session}
+          initialMessages={initialMessages}
+        />
+      </div>
+      <ChatInput chatId={chatId} />
     </>
-  )
+  );
 }
 
-export default ChatPage
+export default ChatPage;
--- app/(user)/chat/layout.tsx
+++ app/(user)/chat/layout.tsx
@@ -4,7 +4,7 @@ export default function ChatsLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex-1 w-full flex flex-col max-w-6x1 mx-auto">
+    <div className="flex-1 w-full flex flex-col max-w-6xl mx-auto">
       {children}
     </div>
   );
